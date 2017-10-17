a7776cf838f82188f64ab9fac3667a1a99f34f9d
scott-adamson1975 <32680457+scott-adamson1975@users.noreply.github.com>
2017-10-17 07:15:55 +0200

    Update flystick_config.py
    add elevator trim on hat up/down
    
    change scaling to .75 on sticks to match futaba 100% epa, add hotas throttle and sliders

--- flystick_config.py
+++ flystick_config.py
@@ -10,30 +10,31 @@ stick = Joystick(0)
 
 # aileron trim, hat side-to-side axis
 ail_trim = stick.hat_switch(hat=0, axis=0, positions=41, initial=20)
+ele_trim = stick.hat_switch(hat=0, axis=1, positions=41, initial=20)
 
 # Raspberry Pi GPIO pin where to output the PPM signal.
 # Pin map: http://wiki.mchobby.be/images/3/31/RASP-PIZERO-Correspondance-GPIO.jpg
 # (Connect this pin to the RC transmitter trainer port.)
-PPM_OUTPUT_PIN = 18
+PPM_OUTPUT_PIN = 24
 
 # Output (PPM) channels.
 CHANNELS = (
     # channel 1: aileron with trim
-    stick.axis(0) + ail_trim * 0.5,
+    stick.axis(0) + ail_trim * 0.75,
     # a more elaborate example with reverse, offset, weight and trim:
     #(-stick.axis(0) + 0.1) * 0.7 + ail_trim * 0.5,
     # channel 2: elevator (reversed)
-    -stick.axis(1),
+    stick.axis(1) + ele_trim * 0.75,
     # channel 3: throttle (reversed)
-    -stick.axis(2),
+    stick.axis(2) * 0.75,
     # channel 4: flight mode
     # hat up-down axis, 5 states to match scrollphat vertical resolution
-    stick.hat_switch(hat=0, axis=1, positions=5),
+    stick.axis(4) * 0.75,
     # channels 5-8: buttons demo
+    stick.axis(3) * 0.75,
     stick.button(0),
     stick.button(1),
     stick.button(2),
-    stick.button(3),
 )
 
 # dual-channel display component

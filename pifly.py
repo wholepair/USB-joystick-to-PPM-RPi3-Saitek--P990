e7d01fc4576678776bd2380a1f96e635b349d3b4
scott-adamson1975 <32680457+scott-adamson1975@users.noreply.github.com>
2017-11-03 07:43:24 +0100

    Update pifly.py

--- pifly.py
+++ pifly.py
@@ -1,100 +1,98 @@
-from pygame import joystick, event
-from pygame import QUIT, JOYAXISMOTION, JOYBALLMOTION, JOYHATMOTION, JOYBUTTONUP, JOYBUTTONDOWN
-import pygame
+"""py joy  fly stick"""
 import thread
+import logging
 import time
-import logging 
+import pygame
+from pygame import joystick, event, JOYAXISMOTION
+from pygame import JOYBUTTONUP, JOYBUTTONDOWN
 
 try:
     import pigpio
-except ImportError as e:
-    logging.warn(e, exc_info=True)
+except ImportError as err:
+    logging.warn(err, exc_info=True)
     logging.warn("Failed to load pigpio library, running in debug mode")
     pigpio = None
 
-_pi = None
-_waves = [None,None,None]
-_running = False
-_pi_gpio = 1 << 24
-_joyA = [0,1,2,3]
-_joyB = [4,5,6,7]
+RUNNING = False
+PI_PPM = 24
+PI_GPIO = 1 << PI_PPM
+JOYA = [0, 1, 2, 3]
+JOYB = [4, 5, 6, 7]
 
-def readjoythread():
-    output = [0,0,0,0,0,0,0,0]
-    
+def readjoythread(pinst, waves):
+    """Read joystick loop and pass result onto processor"""
+    output = [0, 0, 0, 0, 0, 0, 0, 0]
     joystick.init()
     joystick.Joystick(0).init()
     for evt in pygame.event.get():
         time.sleep(.02)
 
-    for x in range(0, len(_joyA)):
-        if _joyA[x] >-1:
-            output[_joyA[x]] = round(joystick.Joystick(0).get_axis(x),4)
-            if output[_joyA[x]] == None:
-                output[_joyA[x]] = 0
-    for chan in _joyB:
+    for i in range(0, len(JOYA)):
+        if JOYA[i] > -1:
+            output[JOYA[i]] = round(joystick.Joystick(0).get_axis(i), 4)
+            if output[JOYA[i]] is None:
+                output[JOYA[i]] = 0
+    for chan in JOYB:
         output[chan] = -1
-    
-    processoutput(output[:])
 
-    while _running:
+    processoutput(output[:], pinst, waves)
+
+    while RUNNING:
         haschanged = False
-        e = event.wait();  
-        if e.type == JOYAXISMOTION: 
-            if e.axis < len(_joyA) and _joyA[e.axis] > -1:
-                 haschanged = True
-                 output[_joyA[e.axis]] = round(e.value,4)
-        elif e.type == JOYBUTTONUP: 
-            if e.button < len(_joyB) and _joyB[e.button] > -1:
-                 output[_joyB[e.button]] = -1
-                 haschanged = True
-        elif e.type == JOYBUTTONDOWN: 
-            if e.button < len(_joyB) and _joyB[e.button] > -1:
-                 output[_joyB[e.button]] = 1
-                 haschanged = True
+        evt = event.wait()
+        if evt.type == JOYAXISMOTION:
+            if evt.axis < len(JOYA) and JOYA[evt.axis] > -1:
+                output[JOYA[evt.axis]] = round(evt.value, 4)
+                haschanged = True
+        elif evt.type == JOYBUTTONUP or evt.type == JOYBUTTONDOWN:
+            if evt.button < len(JOYB) and JOYB[evt.button] > -1:
+                output[JOYB[evt.button]] = -1 if evt.type == JOYBUTTONUP else 1
+                haschanged = True
         if haschanged:
-            processoutput(output[:])
+            processoutput(output[:], pinst, waves)
 
-def processoutput(channels):
+def processoutput(channels, pinst, waves):
+    """process outout and send wave to pigpio"""
     if pigpio:
         pulses, pos = [], 0
         for value in channels:
             # calibrated with Taranis to [-99.6..0..99.4]
-            us = int(round(1333 + 453 * value))
-            pulses += [pigpio.pulse(0, _pi_gpio, 300),
-                       pigpio.pulse(_pi_gpio, 0, us - 300)]
-            pos += us
+            uss = int(round(1333 + 453 * value))
+            pulses += [pigpio.pulse(0, PI_GPIO, 300),
+                       pigpio.pulse(PI_GPIO, 0, uss - 300)]
+            pos += uss
 
-            pulses += [pigpio.pulse(0, _pi_gpio, 300),
-                       pigpio.pulse(_pi_gpio, 0, 20000 - 300 - pos - 1)]
+            pulses += [pigpio.pulse(0, PI_GPIO, 300),
+                       pigpio.pulse(PI_GPIO, 0, 20000 - 300 - pos - 1)]
 
-            _pi.wave_add_generic(pulses)
-            _waves.append(_pi.wave_create())
-            _pi.wave_send_using_mode(waves[-1], pigpio.WAVE_MODE_REPEAT_SYNC)
+            pinst.wave_add_generic(pulses)
+            waves.append(pinst.wave_create())
+            pinst.wave_send_using_mode(waves[-1], pigpio.WAVE_MODE_REPEAT_SYNC)
 
             last, waves = waves[0], waves[1:]
             if last:
-                _pi.wave_delete(last)
-    else :
+                pinst.wave_delete(last)
+    else:
         logging.warn(channels)
-    
 
 def main():
-    global _pi, _waves
+    """Main Entry point"""
     if pigpio:
-        _pi = pigpio.pi('test')
-        _pi.set_mode(PPM_OUTPUT_PIN, pigpio.OUTPUT)
-        _pi.wave_add_generic([pigpio.pulse(pi_gpio, 0, 2000)])
+        pinst = pigpio.pi('test')
+        pinst.set_mode(PI_PPM, pigpio.OUTPUT)
+        pinst.wave_add_generic([pigpio.pulse(PI_GPIO, 0, 2000)])
         # padding to make deleting logic easier
-        _waves = [None, None, _pi.wave_create()]
-        _pi.wave_send_repeat(waves[-1])
+        waves = [None, None, pinst.wave_create()]
+        pinst.wave_send_repeat(waves[-1])
 
     pygame.init()
-    thread.start_new_thread(readjoythread, ())
-    while _running:
+    thread.start_new_thread(readjoythread, (pinst, waves))
+    while RUNNING:
         time.sleep(.02)
 
+    pygame.quit()
+
 if __name__ == '__main__':
-    _running = True
+    RUNNING = True
     #signal.signal(signal.SIGCHLD, signal.SIG_IGN)
     main()
